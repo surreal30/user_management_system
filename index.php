4955658ef663a62d2b1ac32fa19e267d2b308d4e
surreal <2002vaibhavlakhera3@gmail.com>
2023-07-27 23:35:38 +0530

    add index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>User Management System</title>
+</head>
+<body>
+	<style>
+		a{
+			color: #FFFFFF;
+		}
+		ul{
+			background-color: #000000;
+		}
+		li{
+			color: #FFFFFF;
+			display: inline;
+			margin: 30px;
+		}
+	</style>
+
+	<hr>
+		<ul>
+			<li> <a href="home.php"> Home </a> </li>
+			<li> <a href="addUser.php"> Add user </a> </li>
+			<li> <a href="listUser.php"> list user </a> </li>
+		</ul>
+	<hr>
+
+	<p style="margin-top: 40px;"> <center> <h1>Welcome to User Management System.</h1> </center> </p>
+
+</body>
+</html>
