07af1b05900b50cde76905b47f3b834bb81fc385
surreal <2002vaibhavlakhera3@gmail.com>
2023-07-27 19:39:46 +0530

    Create home.php.

---
+++ home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>User Management System</title>
+</head>
+<body>
+<p>This is the first page.</p>
+</body>
+</html>
