c43bf659e93695a7461029dffa49d1c10f35c2fc
surreal <2002vaibhavlakhera3@gmail.com>
2023-07-28 18:07:59 +0530

    Updated links in the navigation bar.

--- index.php
+++ index.php
@@ -6,8 +6,29 @@
 	<title>User Management System</title>
 </head>
 <body>
-<p>This is the first page.</p>
-Navigation bar <br>
-Home   Add user    list user <br>
+	<style>
+		a{
+			color: #FFFFFF;
+		}
+		ul{
+			background-color: #000000;
+		}
+		li{
+			color: #FFFFFF;
+			display: inline;
+			margin: 30px;
+		}
+	</style>
+
+	<hr>
+		<ul>
+			<li> <a href="index.php"> Home </a> </li>
+			<li> <a href="add_user.php"> Add user </a> </li>
+			<li> <a href="list_user.php"> list user </a> </li>
+		</ul>
+	<hr>
+
+	<p style="margin-top: 40px;"> <center> <h1>Welcome to User Management System.</h1> </center> </p>
+
 </body>
 </html>
