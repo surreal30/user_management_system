d48093a72aee82cbdcaaf8c52ab8b414b047c6ea
surreal <2002vaibhavlakhera3@gmail.com>
2023-08-02 17:53:16 +0530

    Added welcome user message and session. If opened without any user logged in will redirect to login.php

--- index.php
+++ index.php
@@ -20,11 +20,25 @@
 		}
 	</style>
 
+	
+	<?php
+		session_start();
+		session_regenerate_id();
+		if(!isset($_SESSION['user']))
+		{
+			header("location: login.php");
+		}
+		$user = $_SESSION['user'];
+	?>
+
 	<hr>
 		<ul>
 			<li> <a href="index.php"> Home </a> </li>
 			<li> <a href="add_user.php"> Add user </a> </li>
 			<li> <a href="list_user.php"> list user </a> </li>
+			<li> <?php echo "Welcome ", $user; ?> </li>
+			<li> <a href="logout.php"> Logout </a> </li>
+
 		</ul>
 	<hr>
 
