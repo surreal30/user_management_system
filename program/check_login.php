518218b8e7c54ff75882b561af7f35607e83bd7e
surreal <2002vaibhavlakhera3@gmail.com>
2023-08-14 12:43:36 +0530

    Feat: Check user's authenticity from login.php.

--- program/check_login.php
+++ program/check_login.php
@@ -16,3 +16,11 @@
 			header("location: login.php");
 		}
 	}
+
+	if(isset($_SESSION['HTTP_USER_AGENT']))
+	{
+		if($_SESSION['HTTP_USER_AGENT'] != md5($_SERVER['HTTP_USER_AGENT']))
+		{
+			die("User not identified");
+		}
+	}
