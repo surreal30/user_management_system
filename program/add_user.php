924743e97a0215564ad8d177c271b5aad4e60cd1
surreal <2002vaibhavlakhera3@gmail.com>
2023-08-14 12:07:56 +0530

    Feat: Add bulk upload feature. Takes in json file and uploads the data in database.

--- program/add_user.php
+++ program/add_user.php
@@ -4,6 +4,8 @@
 	{
 		die('Access denied');
 	}
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -62,6 +64,13 @@
 	</p>
 
 	<p>
+	 	<form action="add_user.php" method="post" enctype="multipart/form-data">
+			<input type="file" name="data">
+			<input type="submit" name="bulk_upload" value="Upload">
+		</form>
+	</p>
+
+	<p>
 		<?php
 
 			$serverName = getenv('MYSQL_HOST');
@@ -79,24 +88,75 @@
 
 			if(isset($_POST['first_name']))
 			{
-				$queryStatement = $mysqli->prepare("INSERT INTO users (id, first_name, last_name, email, created_at, updated_at, phone_no, password) value (0, ?, ?, ?, NOW(), NOW(), ?, ?)");
-				$firstName = $_POST['first_name'];
-				$lastName = $_POST['last_name'];
-				$email = $_POST['email'];
-				$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-				$phoneNo = $_POST['phone_no'];
-
-				try
+				if(preg_match('/^[A-Z][a-z]+$/', $_GET['first_name']) && preg_match('/^[A-Z][a-z]+$/', $_POST['last_name']) && preg_match('/^[a-zA-Z0-9][a-zA-Z0-9]*([_#$+*&{}=%^\/\-.!]*[a-zA-Z0-9])*@[a-zA-Z]*\.\w[a-zA-Z]*\.?\w[a-zA-Z]*$/', $_POST['email']) && preg_match('/^[0-9a-zA-z\-+$%^*&_#@!]+$/', $_POST['password']) && preg_match('/^[0-9]+$/', $_POST['phone_no']))
 				{
-					$queryStatement->bind_param("sssss", $firstName, $lastName, $email, $phoneNo, $password);
-					$queryStatement->execute();
-				}
-				catch(\Throwable $e)
-				{
-					die("Error occured $e");
-				}
+					$queryStatement = $mysqli->prepare("INSERT INTO users (id, first_name, last_name, email, created_at, updated_at, phone_no, password) value (0, ?, ?, ?, NOW(), NOW(), ?, ?)");
+					$firstName = $_POST['first_name'];
+					$lastName = $_POST['last_name'];
+					$email = $_POST['email'];
+					$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+					$phoneNo = $_POST['phone_no'];
 
-				echo " <h3> User $firstName $lastName has been created and added to database. </h3>";
+					try
+					{
+						$queryStatement->bind_param("sssss", $firstName, $lastName, $email, $phoneNo, $password);
+						$queryStatement->execute();
+					}
+					catch(\Throwable $e)
+					{
+						die("Error occured $e");
+					}
+
+					echo " <h3> User $firstName $lastName has been created and added to database. </h3>";	
+				}
+				else
+				{
+					die("Please enter correct information");
+				}
+				
+			}
+
+			if(isset($_POST['bulk_upload']))
+			{
+				if(is_uploaded_file($_FILES['data']['tmp_name']))
+				{
+					if($_FILES['data']['type'] == 'application/json')
+					{
+						$userFile = $_FILES['data']['tmp_name'];
+						$fileContent = file_get_contents($userFile);
+						try
+						{
+							$fileData = json_decode($fileContent, TRUE);
+						}
+						catch(\Throwable $e)
+						{
+							die("Error occured $e. Check if uploaded file is valid json file or not.");
+						}
+
+						foreach ($fileData as $user)
+						{
+							$firstName = $user['first_name'];
+							$lastName = $user['last_name'];
+							$email = $user['email_id'];
+							$password = password_hash($user['password'], PASSWORD_DEFAULT);
+							$phoneNo = $user['mobile_no'];
+
+							$queryStatement = $mysqli->prepare("INSERT INTO users (id, first_name, last_name, email, created_at, updated_at, phone_no, password) value (0, ?, ?, ?, NOW(), NOW(), ?, ?)");
+
+							try
+							{
+								$queryStatement->bind_param("sssss", $firstName, $lastName, $email, $phoneNo, $password);
+								$queryStatement->execute();
+							}
+							catch(\Throwable $e)
+							{
+								die("Error occured $e");
+							}
+						}
+						echo "All the users have been created";
+					}	
+				}
+				
 			}
 			
 		?>
