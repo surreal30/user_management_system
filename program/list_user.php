df07f59484d8b13a8acb428777edbdbe51b83bb6
surreal <2002vaibhavlakhera3@gmail.com>
2023-09-02 18:46:53 +0530

    feat: display empty table when page does not exist.

--- program/list_user.php
+++ program/list_user.php
@@ -4,7 +4,6 @@
 	{
 		die('Access denied');
 	}
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -140,20 +139,20 @@
 					$limit = $_GET['count'];
 				}
 
-
 				$countQuery = $mysqli->query('SELECT COUNT(*) FROM users');
 				$totalRows = $countQuery->fetch_assoc();
 				$rowCount = $totalRows['COUNT(*)'];
 
 				$totalPages = ceil($rowCount/$limit);
 
-				if($currentPage > $totalPages)
+				if($currentPage > $totalPages || !(preg_match('<^\w[0-9]*$>', $currentPage)))
 				{
-					echo "<center><h4><a class='page-link pt-2' href='http://localhost:8080/admin/users?page=$totalPages&count=$limit'> No such page exists. Click here to open last page of users </a></h4></center>";
-					exit();
+					$offset = 0;
 				}
-
-				$offset = ($currentPage - 1) * $limit;
+				else
+				{
+					$offset = ($currentPage - 1) * $limit;
+				}
 				$selectRowQuery = $mysqli->prepare('SELECT * FROM users LIMIT ?, ?');
 				$selectRowQuery->bind_param("ss", $offset, $limit);
 				$selectRowQuery->execute();
@@ -169,14 +168,45 @@
 					while ($currentRow = $results->fetch_assoc())
 					{
 						echo "<tr class='text-center' scope='row'> <td>", $currentRow['first_name'], " ", $currentRow['last_name'], "</td> <td>", $currentRow['email'], "</td> <td>", $currentRow['created_at'], "</td> <td>", $currentRow['updated_at'], "</td> <td>", $currentRow['phone_no'], "</td> <td style='text-align: center' > <a href='http://localhost:8080/admin/users/", $currentRow['id'], "/edit'> <span> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'> <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/> <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/> </svg> </span> </a> </td> <td style='text-align: center'> <a href='http://localhost:8080/admin/users/", $currentRow['id'], "/delete' <span> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='red' class='bi bi-trash' viewBox='0 0 16 16'> <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z'/> <path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z'/> </svg> </span> </a> </td> </tr>" ;
-					
 					}
 
 					echo "</tbody></table>";
 					echo "<nav>";
 					echo "<div class='d-flex align-items-center justify-content-center gap-2'>";
 
-					if($currentPage == 1)
+					if($currentPage > $totalPages || !(preg_match('<^\w[0-9]*$>', $currentPage)))
+					{
+						$currentPage = 1;
+						$nextPage = $currentPage + 1;
+						$thirdPage = $nextPage + 1;
+						echo <<<PAGEINATION
+							
+								<ul class='pagination pagination-circle mb-0'>
+									<li class='page-item disabled'>
+										<a class='page-link' href='http://localhost:8080/admin/users?page=1&count=$limit' >First</a>
+									</li> 
+									<li class='page-item disabled'>
+										<a class='page-link' href='http://localhost:8080/admin/users?page=1&count=$limit'><span aria-hidden="true">&laquo;</span></a>
+									</li>
+									<li class='page-item active'> 
+										<a class='page-link' href='http://localhost:8080/admin/users?page=$currentPage&count=$limit'> $currentPage </a>
+									</li>
+									<li class='page-item'>
+										<a class='page-link' href='http://localhost:8080/admin/users?page=$nextPage&count=$limit'> $nextPage </a>
+									</li>
+									<li class='page-item'>
+										<a class='page-link' href='http://localhost:8080/admin/users?page=$thirdPage&count=$limit'> $thirdPage</a>
+									</li>
+									<li class='page-item'>
+										<a class='page-link' href='http://localhost:8080/admin/users?page=$nextPage&count=$limit'><span aria-hidden="true">&raquo;</span></a>
+									</li>
+									<li class='page-item'>
+										<a class='page-link' href='http://localhost:8080/admin/users?page=$totalPages&count=$limit'>Last</a>
+									</li>
+								</ul>
+						PAGEINATION;	
+					}
+					elseif($currentPage == 1)
 					{
 						$nextPage = $currentPage + 1;
 						$thirdPage = $nextPage + 1;
