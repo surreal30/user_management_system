673014f9a642582c407e8efe2909fd78b2f55ff8
surreal <2002vaibhavlakhera3@gmail.com>
2023-10-28 21:00:58 +0530

    feat: check uri by checking individual array element

--- public/index.php
+++ public/index.php
@@ -10,35 +10,35 @@
 
 	$requestURI = (isset($_SERVER['REQUEST_URI'])? explode('/', $URI) : null );
 
-	if( count($requestURI) == 2 && in_array("admin", $requestURI))
+	if( count($requestURI) == 2 && $requestURI[1] == 'admin')
 	{
 		$route = "/homepage.php";
 	}
-	elseif( count($requestURI) == 3 && in_array("login", $requestURI))
+	elseif( count($requestURI) == 3 && $requestURI[1] == 'admin' && $requestURI[2] == 'login')
 	{
 		$route = "/login.php";
 	}
-	elseif( count($requestURI) == 3 && in_array("logout", $requestURI))
+	elseif( count($requestURI) == 3 && $requestURI[1] == 'admin' && $requestURI[2] == 'logout')
 	{
 		$route = "/logout.php";
 	}
-	elseif( count($requestURI) == 3 && in_array("users", $requestURI))
+	elseif( count($requestURI) == 3 && $requestURI[1] == 'admin' && $requestURI[2] == 'users')
 	{
 		$route = "/list_user.php";
 	}
-	elseif( count($requestURI) == 3 && str_contains($requestURI[2], "?"))
+	elseif( count($requestURI) == 3 && $requestURI[1] == 'admin' && str_contains($requestURI[2], "?"))
 	{
 		$route = "/list_user.php";
 	}
-	elseif( count($requestURI) == 4 && in_array("add", $requestURI))
+	elseif( count($requestURI) == 4 && $requestURI[1] == 'admin' && $requestURI[2] == 'users' && $requestURI[3] == 'add')
 	{
 		$route = "/add_user.php";
 	}
-	elseif( count($requestURI) == 5 && in_array("edit", $requestURI) && preg_match('<^\d[0-9]*$>', $requestURI[3]))
+	elseif( count($requestURI) == 5 && $requestURI[1] == 'admin' && $requestURI[2] == 'users' && $requestURI[4] == 'edit')
 	{
 		$route = "/edit_user.php";
 	}
-	elseif( count($requestURI) == 5 && in_array("delete", $requestURI))
+	elseif( count($requestURI) == 5 && $requestURI[1] == 'admin' && $requestURI[2] == 'users' && $requestURI[4] == 'delete')
 	{
 		$route = "/delete_user.php";
 	}
