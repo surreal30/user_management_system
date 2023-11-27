3a09c7d505569be467da1f6065987c6504896278
surreal <2002vaibhavlakhera3@gmail.com>
2023-11-27 22:50:42 +0530

    feat: create AuthenticationController for login and logout

--- public/index.php
+++ public/index.php
@@ -1,30 +1,40 @@
 <?php
+
 require_once("src/UserController.php");
+require_once("src/AuthenticationController.php");
 
 $url = parse_url($_SERVER['REQUEST_URI']);
 
 $urlMap = [
-	"/admin"                 => ["/src/homepage.php", null],
-	"/admin/login"           => ["/src/login.php", null],
-	"/admin/logout"          => ["/src/logout.php", null],
-	"/admin/users"           => [null, "listUser"],
-	"/admin/users/add"       => [null, "addUser"],
-	"/admin/users/edit"      => [null, "editUser"],
-	"/admin/users/delete"    => [null, "deleteUser"]
+	"/admin"                 => ["version" => "v1", "value" => "/src/homepage.php"],
+	"/admin/login"           => ["version" => "v2", "value" => ["AuthenticationController", "login"]],
+	"/admin/logout"          => ["version" => "v2", "value" => ["AuthenticationController", "logout"]],
+	"/admin/users"           => ["version" => "v2", "value" => ["UserController", "listUser"]],
+	"/admin/users/add"       => ["version" => "v2", "value" => ["UserController", "addUser"]],
+	"/admin/users/edit"      => ["version" => "v2", "value" => ["UserController", "editUser"]],
+	"/admin/users/delete"    => ["version" => "v2", "value" => ["UserController", "deleteUser"]],
 ];
 
-if(isset($urlMap[$url['path']][1]))
+if(isset($urlMap[$url['path']]))
 {
-	$user = new UserController();
-	
-	$function = $urlMap[$url['path']][1];
+	if($urlMap[$url['path']]['version'] == "v1")
+	{
+		require __dir__ . $urlMap[$url['path']]['value']; // $route;
+	}
 
-	$user->$function();
+	elseif($urlMap[$url['path']]['version'] == "v2")
+	{
+		$controller = $urlMap[$url['path']]['value'][0];
+		
+		$user = new $controller();
+
+		$function = $urlMap[$url['path']]['value'][1];
+
+		$user->$function();
+	}
 }
 
 else
 {
-	$route = isset($urlMap[$url['path']][0]) ? $urlMap[$url['path']][0] : "/src/404.php";
-
-	require __dir__ . $route;
+	require __dir__ . "/src/404.php";
 }
